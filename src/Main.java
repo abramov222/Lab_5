df93daf3cb8073b9e58b2ce0d7a98e9d0206f0ab
abramov222 <ilyabramov22222@gmail.com>
2025-10-27 08:36:40 +0700

    feat: add 1 task to the Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.util.*;
 
-public class Main {
-	
+class EvenThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println("Четный поток: " + i);
+            }
+        }
+    }
 }
 
+class OddRunnable implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 != 0) {
+                System.out.println("Нечетный поток: " + i);
+            }
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Задание 1: Базовые потоки ===");
+        Thread evenThread = new EvenThread();
+        Thread oddThread = new Thread(new OddRunnable());
+        
+        evenThread.start();
+        oddThread.start();
+        
+        try {
+            evenThread.join();
+            oddThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
